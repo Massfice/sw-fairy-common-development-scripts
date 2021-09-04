b03c4469dc2f44e50af0093ae181fcfc860a1d4f
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-09-04 20:31:02 +0200

    Add pm2 to run script

--- src/run/index.ts
+++ src/run/index.ts
@@ -1,9 +1,27 @@
-import RunOptions from '../types/runOptions';
 import gulp from 'gulp';
 import path from 'path';
+import { exec } from 'child_process';
+
+import RunOptions from '../types/runOptions';
 
 const run = (options: RunOptions) => {
     const ecosystemPath = path.join(options.dirname, 'ecosystem.config.js');
+
+    exec(`pm2 start ${ecosystemPath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(error);
+
+            throw error;
+        }
+
+        if (stderr) {
+            console.error(stderr);
+
+            throw new Error(stderr);
+        }
+
+        console.log(stdout);
+    });
 
     return gulp.watch(ecosystemPath, (done) => {
         console.log('Ecosystem changed');
